0e00ac4ed06a840298f46238c64530451a113761
Ryo Ota <nwtgck@gmail.com>
2017-11-02 13:45:07 +0900

    [impl] Implement basic step-by-step scrambler

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React      from 'react';
 import * as ReactDOM   from 'react-dom';
+import {List}          from 'immutable'
 
 // Typed Props
 interface Props {
@@ -9,14 +10,54 @@ interface Props {
 // Typed State
 interface State {
     algorithm: string;
+    currRotateCodesIdx: number;
 }
 
 class App extends React.Component<Props, State> {
   constructor(props){
     super(props);
     this.state = {
-      algorithm: '',
+        algorithm         : "U L' F U R L' U R D F D' B D L B F L B' D L' B L D L' U",
+        currRotateCodesIdx: 0
     }
+  }
+
+  onChangeAlgorithm(e: any){
+      // Get input
+      const algorithm = e.target.value;
+
+      const currRotateCodesIdx = (this.state.currRotateCodesIdx < algorithm.length)?
+          this.state.currRotateCodesIdx: algorithm.length - 1;
+
+      // Change state
+      this.setState({
+          algorithm         : algorithm,
+          currRotateCodesIdx: currRotateCodesIdx
+      });
+  }
+
+    /**
+     * Generate rotate-codes
+     * @returns {List<string>}
+     */
+  getRotateCodes(): List<string>{
+      // Generate rotate codes
+      const rotateCodes =  List(this.state.algorithm.split(' '));
+      return rotateCodes;
+  }
+
+    /**
+     * Increment/Decrement current rotate-codes index
+     * @param {number} inc
+     */
+  incCurrRotateCodesIdx(inc: number){ // NOTE: inc can be negative value
+      const inced = this.state.currRotateCodesIdx + inc;
+
+      if(0 <= inced && inced < this.state.algorithm.length){
+          this.setState({
+              currRotateCodesIdx: inced
+          });
+      }
   }
 
   render () {
@@ -26,13 +67,29 @@ class App extends React.Component<Props, State> {
         <p>
             <input
                 defaultValue={this.state.algorithm}
-                onChange={(e) => this.setState({algorithm: e.target.value})}
+                onChange={this.onChangeAlgorithm.bind(this)}
                 style={{width: '600px'}}
                 placeholder="Input algorithm"
             />
         </p>
 
-          <img src={`http://cube.crider.co.uk/visualcube.php?fmt=svg&size=300&pzl=3&alg=${this.state.algorithm}`} />
+          <img src={`http://cube.crider.co.uk/visualcube.php?fmt=svg&size=300&pzl=3&alg=${this.getRotateCodes.bind(this)().take(this.state.currRotateCodesIdx).join("")}`} />
+          <button onClick={() => this.incCurrRotateCodesIdx.bind(this)(-1)}>{"<"}</button>
+          <button onClick={() => this.incCurrRotateCodesIdx.bind(this)(+1)}>{">"}</button>
+
+          <p>
+              {
+                  this.getRotateCodes.bind(this)().take(this.state.currRotateCodesIdx).map((e, idx) => {
+                      return <span key={idx} style={{color: 'black', fontWeight: 'bold', fontSize: '20px'}}>{e}</span>
+                  })
+              }
+              {
+                  this.getRotateCodes.bind(this)().skip(this.state.currRotateCodesIdx).map((e, idx) => {
+                      return <span key={idx} style={{color: 'gray'}}>{e}</span>
+                  })
+              }
+          </p>
+
       </div>
     );
   }
